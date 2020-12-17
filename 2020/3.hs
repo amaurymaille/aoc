6f513773e318913cdb439d5da1ff4cf92b82c49a
Amaury Maillé <amaury.maille@gmail.com>
2020-12-17 11:27:45 +0100

    Second part of challenge 3

--- 2020/3.hs
+++ 2020/3.hs
@@ -8,16 +8,34 @@ printInput (x:xs) = print x >>
                     printInput xs
 printInput [] = return ()
 
+-- Keep all N element of a list (starting with the first)
+allN :: [a] -> Int -> [a]
+allN lst n = allNRec lst n 0
+    where
+        allNRec :: [a] -> Int -> Int -> [a]
+        allNRec (x:xs) n depth = 
+            if depth == 0 then
+                let res = allNRec xs n (n - 1) in
+                (x:res)
+            else
+                allNRec xs n (depth - 1)
+        allNRec [] _ _ = []
+
 processLines :: [String] -> Int
-processLines lines = processLineByLine lines 0 0
+processLines lines = product (map (\(right, down) -> processOneSlope lines right down) [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)])
     where 
-        processLineByLine :: [String] -> Int -> Int -> Int
-        processLineByLine (x:xs) depth amount = 
-            let position = mod (depth * 3) (length x)
+        processOneSlope :: [String] -> Int -> Int -> Int
+        processOneSlope lst shiftRight shiftDown =
+            let filtered = allN lst shiftDown in
+                processLineByLine filtered shiftRight 0 0
+
+        processLineByLine :: [String] -> Int -> Int -> Int -> Int
+        processLineByLine (x:xs) shift depth amount = 
+            let position = mod (depth * shift) (length x)
                 isTree = (x!!position) == '#'
             in
-                processLineByLine xs (depth + 1) (if isTree then amount + 1 else amount)
-        processLineByLine [] _ amount = amount
+                processLineByLine xs shift (depth + 1) (if isTree then amount + 1 else amount)
+        processLineByLine [] _ _ amount = amount
 
 main :: IO ()
 main = getInput >>= \lines -> let res = processLines lines in print res
